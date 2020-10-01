654ff554213844c929a71f45827392e2bc8c09ae
Ziad <ziad1991@hotmail.co.uk>
2020-10-01 15:23:11 +0100

    Use hooks to change query on search bar for api call

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 import Axios from 'axios'
 
 function App() {
+  const [query, setQuery] = useState('')
 
   const APP_ID = '0992780d'
   const APP_KEY = '5e5d73059b0a4681aa46832ad940e2e7'
-  const baseUrl = `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`
+  const baseUrl = `https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`
 
   const fetchData = async () => {
     const result = await Axios.get(baseUrl)
@@ -17,12 +18,16 @@ function App() {
     e.preventDefault()
     fetchData()
   }
-  
+
+  const queryChange = (e) => {
+    setQuery(e.target.value)
+  }
+
   return (
     <div className="App">
       <h1>Recipe Search</h1>
       <form className="search-form" onSubmit={handleCall}>
-          <input type="text" placeholder="Search for some food" autoComplete="off"/>
+          <input type="text" placeholder="Search for some food" autoComplete="off" onChange={queryChange}/>
           <input type="submit" value="Search"/>
       </form>
     </div>
