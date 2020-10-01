b590973ebe7dc69efabc21389e81b490075e2853
Ziad <ziad1991@hotmail.co.uk>
2020-10-01 15:38:06 +0100

    Input recipe data into Recipe component

--- src/components/Recipe.js
+++ src/components/Recipe.js
@@ -1,9 +1,15 @@
 import React from 'react'
 
-const Recipe = () => {
+const Recipe = ({recipe}) => {
+    const {label, image, url, ingredients} = recipe.recipe
     return (
-        <div>
-            Recipe
+        <div className="recipe">
+            <h2>{label}</h2>
+            <img src={image} alt={label}/>
+            <a href={url} target="_blank" rel="noopener noreferrer">
+                URL
+            </a>
+            <button>Ingredients</button>
         </div>
     )
 }
