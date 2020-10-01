e5d5774485ad6bf2148d91c37a404ccef4abb27a
Ziad <ziad1991@hotmail.co.uk>
2020-10-01 15:53:07 +0100

    Add alert component for empty field

--- src/App.js
+++ src/App.js
@@ -3,20 +3,27 @@ import './App.css';
 import Axios from 'axios'
 import {v4 as uuidv4} from 'uuid'
 import Recipe from './components/Recipe'
+import Alert from './components/Alert'
 
 function App() {
   const [query, setQuery] = useState('')
   const [recipes, setRecipes] = useState([])
+  const [alert, setAlert] = useState('')
 
   const APP_ID = '0992780d'
   const APP_KEY = '5e5d73059b0a4681aa46832ad940e2e7'
   const baseUrl = `https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`
 
   const fetchData = async () => {
-    const result = await Axios.get(baseUrl)
-    setRecipes(result.data.hits)
-    console.log(result)
-    setQuery('')
+    if(query !== '') {
+      const result = await Axios.get(baseUrl)
+      setRecipes(result.data.hits)
+      console.log(result)
+      setQuery('')
+    } else {
+      setAlert('Please fill in the field')
+    }
+
   }
 
   const handleCall = (e) => {
@@ -32,6 +39,7 @@ function App() {
     <div className="App">
       <h1>Recipe Search</h1>
       <form className="search-form" onSubmit={handleCall}>
+        {alert !== "" && <Alert alert={alert}/>}
           <input type="text" placeholder="Search for some food" autoComplete="off" onChange={queryChange} value={query}/>
           <input type="submit" value="Search"/>
       </form>
