7887e8be0cb3ae3fcbfd2d7c118f2b8c9e402ead
Ziad <ziad1991@hotmail.co.uk>
2020-10-01 15:11:26 +0100

    Import info from Edamam API

--- src/App.js
+++ src/App.js
@@ -2,6 +2,11 @@ import React from 'react';
 import './App.css';
 
 function App() {
+
+  const APP_ID = '0992780d'
+  const APP_KEY = '5e5d73059b0a4681aa46832ad940e2e7'
+  const baseUrl = `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`
+
   return (
     <div className="App">
       <h1>Recipe Search</h1>
